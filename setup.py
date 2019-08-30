7df767f6724d79bf696d9430cf48503e6bf74c61
Dayal Chand Aichara <dc.aichara@lastroots.com>
2019-08-30 10:27:02 +0900

    V-0.5.2

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 setuptools.setup(
-  name = 'PriceIndices',
-  packages = ['PriceIndices'],
-  version = '0.5.1',
+  name='PriceIndices',
+  packages=['PriceIndices'],
+  version='0.5.2',
   license='MIT',
-  description = 'This package can be useful to get historical price data of cryptocurrencies from CoinMarketCap, and calculate & plot different indicators.',
-  author = 'Dayal Chand Aichara',
-  author_email = 'dc.aichara@gmail.com',
-  url = 'https://github.com/dc-aichara/Price-Indices',
-  download_url = 'https://github.com/dc-aichara/PriceIndices/archive/v-0.5.1.tar.gz',
-  keywords = ['Volatility', 'blockchain', 'cryptocurrency', 'Price', 'trading', 'CoinMarketCap', 'Indices', 'Indicators'],
+  description='A python package to get historical market data of cryptocurrencies from CoinMarketCap, and calculate & plot different indicators.',
+  author='Dayal Chand Aichara',
+  author_email='dc.aichara@gmail.com',
+  url='https://github.com/dc-aichara/Price-Indices',
+  download_url='https://github.com/dc-aichara/PriceIndices/archive/v-0.5.2.tar.gz',
+  keywords=['Volatility', 'blockchain', 'cryptocurrency', 'Price', 'trading', 'CoinMarketCap', 'Indices', 'Indicators'],
   install_requires=['requests', 'pandas', 'numpy', 'matplotlib'],
   classifiers=[
     'Development Status :: 4 - Beta',
